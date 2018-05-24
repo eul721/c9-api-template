c87f55f64f17a28b2d97c4f72c9cd3260070eb20
Jack Chao <eul721@gmail.com>
2018-05-24 00:41:39 +0000

    Initial commit

---
+++ func/index.js
@@ -0,0 +1,11 @@
+exports.handler = (event, context, callback) => {
+    console.log(event);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "My Header Value"
+        },
+        body: JSON.stringify({ "message": "Hello World!" })
+    };
+    callback(null, response);
+};
